e12408364538dfd1dbe6e78ecdd070e873de7a97
Barak <barakm@codevalue.com>
2023-03-11 12:14:08 +0200

    add env

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,1 +1,8 @@
-export const environment = {};
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:3000/api',
+  maxItemsPerPage: 10,
+  featureFlags: {
+    enableNewFeature: true,
+  },
+};
